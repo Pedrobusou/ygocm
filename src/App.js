8c3c56254cc3b39a86676c4b806099aa4e5442e8
Pedro Ramos <pedro@insendi.com>
2020-04-19 13:33:40 +0100

    Call cards enpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CardList from './components/CardList';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [cards, setCards] = useState([]);
+
+  const endpoint = {
+    single: 'https://db.ygoprodeck.com/api/v5/cardinfo.php?name=Dark%20Magician',
+    all: 'https://db.ygoprodeck.com/api/v5/cardinfo.php'
+  }
+
+  useEffect(() => {
+    if (!cards.length) {
+      (async () => {
+        const response = await axios(endpoint.all);
+        setCards(response.data);
+      })();
+    }
+  })
+
+  return <CardList cards={cards} />;
 }
+
 
 export default App;
