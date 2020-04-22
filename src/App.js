89d56d07ef235cbde4efcc6489bd385674c23d9a
Pedro Ramos <pedro@insendi.com>
2020-04-22 23:31:16 +0100

    Create base header & add it to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import CardList from './components/CardList/CardList';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import CardList from "./components/CardList/CardList";
+import Header from "./components/Header/Header";
 
 const App = () => {
   const [cards, setCards] = useState([]);
 
   const endpoint = {
-    single: 'https://db.ygoprodeck.com/api/v5/cardinfo.php?name=Dark%20Magician',
-    search: 'https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=Crystal',
-    all: 'https://db.ygoprodeck.com/api/v5/cardinfo.php'
-  }
+    single:
+      "https://db.ygoprodeck.com/api/v5/cardinfo.php?name=Dark%20Magician",
+    search: "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=Crystal",
+    all: "https://db.ygoprodeck.com/api/v5/cardinfo.php",
+  };
 
   useEffect(() => {
     if (!cards.length) {
@@ -19,10 +21,14 @@ const App = () => {
         setCards(response.data);
       })();
     }
-  })
+  });
 
-  return <CardList cards={cards} />;
-}
-
+  return (
+    <>
+      <Header />
+      <CardList cards={cards} />
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div>menu</div>
+      <div>header</div>
+      <div>filters</div>
+    </header>
+  );
+};
+
+export default Header;
