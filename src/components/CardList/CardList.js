60de2c71a058abf75921a774edc28fa995bd220e
Pedro Ramos <pedro@insendi.com>
2020-04-22 23:54:56 +0100

    add alt property to dismiss warnings

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './cardList.scss';
+import React from "react";
+import "./cardList.scss";
 
 const CardList = ({ cards }) => {
-    return (
-        <>
-            {cards.map(card => (
-                <a key={card.id} data-name={card.id} href="">
-                    <img className="card-img" src={card.card_images[0].image_url_small} />
-                </a>
-            ))}
-        </>
-    );
-}
+  return (
+    <>
+      {cards.map((card) => (
+        <a key={card.id} data-name={card.id} href="#">
+          <img className="card-img" src={card.card_images[0].image_url_small} alt={card.name} />
+        </a>
+      ))}
+    </>
+  );
+};
 
 export default CardList;
