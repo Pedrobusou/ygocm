ceebd0579af9823a04a656e9e4d614f66b56e123
Pedro Ramos <pedro@insendi.com>
2020-04-24 19:45:22 +0100

    format file

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,14 +1,22 @@
-import React from "react";
-import "./cardList.scss";
+import React from 'react';
+import './cardList.scss';
 
-const CardList = ({ cards }) => {
+const CardList = ({cards}) => {
   return (
     <>
-      {cards.map((card) => (
-        <a key={card.id} data-name={card.id} href="#">
-          <img className="card-img" src={card.card_images[0].image_url_small} alt={card.name} />
-        </a>
-      ))}
+      {cards.length ? (
+        cards.map(card => (
+          <a key={card.id} data-name={card.id} href="#">
+            <img
+              className="card-img"
+              src={card.card_images[0].image_url_small}
+              alt={card.name}
+            />
+          </a>
+        ))
+      ) : (
+        <div className="text-center">No cards found</div>
+      )}
     </>
   );
 };
