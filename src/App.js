5ce39c6caabb69ca6de6e42c3b39421f28f9341a
Pedro Ramos <pedro@insendi.com>
2020-04-24 19:57:29 +0100

    perform variable api call pass function to header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import CardList from "./components/CardList/CardList";
-import Header from "./components/Header/Header";
+import React, {useState} from 'react';
+import './App.css';
+import axios from 'axios';
+import CardList from './components/CardList/CardList';
+import Header from './components/Header/Header';
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const apiSearch = 'https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=';
 
-  const endpoint = {
-    single:
-      "https://db.ygoprodeck.com/api/v5/cardinfo.php?name=Dark%20Magician",
-    search: "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=Crystal",
-    all: "https://db.ygoprodeck.com/api/v5/cardinfo.php",
+  const searchCards = async filters => {
+    let newCards = [];
+
+    try {
+      newCards = (await axios(apiSearch + filters.cardName)).data;
+    } catch (error) {}
+
+    setCards(newCards);
   };
-
-  useEffect(() => {
-    if (!cards.length) {
-      (async () => {
-        const response = await axios(endpoint.search);
-        setCards(response.data);
-      })();
-    }
-  });
 
   return (
     <>
-      <Header />
+      <Header searchCards={searchCards} />
       <CardList cards={cards} />
     </>
   );
