f844a379cd73feafc5c10fca101a54c4b1d649db
Alejandro <alesan.ca3@gmail.com>
2020-04-28 18:54:09 +0100

    Sidemenu added
    First version of the side menu using bootstrap. Next step will be adding a better styling and functionality.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,16 @@ import React from "react";
 import "./header.scss";
 import CardNameFilter from "../filters/CardNameFilter/CardNameFilter";
 
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import MyMenu from "./MyMenu";
+
 const Header = () => {
   return (
     <header>
       <div className="row">
-        <div className="col">
-          <svg
-            className="bi bi-list"
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.5 11.5A.5.5 0 013 11h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 7h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 3h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
+        <MyMenu />
 
         <div className="col">
           <CardNameFilter />
---
+++ src/components/Header/MyMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+const MyMenu = () => {
+  return (
+    <DropdownButton id="dropdown-basic-button" title="Show menu">
+      <Dropdown.Item href="#/action-1">Collection</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Wish list</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Deck creator</Dropdown.Item>
+    </DropdownButton>
+  );
+};
+
+export default MyMenu;
