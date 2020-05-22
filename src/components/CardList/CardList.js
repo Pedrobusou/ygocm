fd874ada12acb3c84b28b733fcd25a922708d12d
Alejandro <alesan.ca3@gmail.com>
2020-05-22 11:59:25 +0100

    New className added

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './cardList.scss';
 import Card from '../Card/Card';
 
-const CardList = ({cards}) => {
+const CardList = ({ cards }) => {
   return (
-    <>
+    <div className="show">
       {cards.length ? (
         cards.map(card => <Card card={card} key={card.id} />)
       ) : (
-        <div className="text-center">No cards found</div>
-      )}
-    </>
+          <div className="text-center">No cards found</div>
+        )}
+    </div>
   );
 };
 
