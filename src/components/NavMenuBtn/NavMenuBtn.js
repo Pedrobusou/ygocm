4d4bda1cc9ea0f73af5498dcdbeda56d4b524d02
Pedro Ramos <pedro@insendi.com>
2020-05-28 23:59:36 +0100

    fix prop names & remove prop

--- src/components/NavMenuBtn/NavMenuBtn.js
+++ src/components/NavMenuBtn/NavMenuBtn.js
@@ -5,11 +5,10 @@ const NavMenuBtn = ({isOpen, setIsOpen}) => {
   return (
     <button
       className={isOpen ? 'nav-menu-btn open' : 'nav-menu-btn'}
-      isOpen={isOpen}
       onClick={() => setIsOpen(!isOpen)}
     >
       <svg
-        class="bi bi-list"
+        className="bi bi-list"
         width="1em"
         height="1em"
         viewBox="0 0 16 16"
@@ -17,7 +16,7 @@ const NavMenuBtn = ({isOpen, setIsOpen}) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
         />
       </svg>
