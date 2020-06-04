3a78940259c2a194e1603e206391bff91399c5ea
Alejandro <alesan.ca3@gmail.com>
2020-06-04 19:32:29 +0100

    useEffect and setTimeout

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './cardList.scss';
 import Card from '../Card/Card';
 
-const CardList = ({cards}) => {
+const CardList = ({ cards }) => {
+  const [printedCards, setPrintedCards] = useState([]);
+  const cardsPerLoad = 5;
+
+  useEffect(() => {
+
+    if (cards.length !== 0 || printedCards.length < cards.length) {
+
+      const timeout = setTimeout(() => {
+        console.log('setTimeout');
+
+        setPrintedCards(cards.slice(printedCards.length, printedCards.length + cardsPerLoad));
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeout);
+      }
+    }
+  });
+
   return (
     <div className="card-list">
       {cards.length ? (
-        cards.map(card => <Card card={card} key={card.id} />)
+        printedCards.map(card => <Card card={card} key={card.id} />)
       ) : (
-        <div className="text-center">No cards found</div>
-      )}
+          <div className="text-center">No cards found</div>
+        )}
     </div>
   );
 };
