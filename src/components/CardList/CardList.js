f54c4221560dcc65913886dc33552b16266ecb55
Pedrobusou <pedrobusou@gmail.com>
2020-06-05 13:55:14 +0100

    remove attemp to lazy load

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './cardList.scss';
 import Card from '../Card/Card';
 
-const CardList = ({ cards }) => {
-  const [printedCards, setPrintedCards] = useState([]);
-  const cardsPerLoad = 5;
-
-  useEffect(() => {
-
-    if (cards.length !== 0 || printedCards.length < cards.length) {
-
-      const timeout = setTimeout(() => {
-        console.log('setTimeout');
-
-        setPrintedCards(cards.slice(printedCards.length, printedCards.length + cardsPerLoad));
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeout);
-      }
-    }
-  });
-
+const CardList = ({cards}) => {
   return (
     <div className="card-list">
       {cards.length ? (
-        printedCards.map(card => <Card card={card} key={card.id} />)
+        cards.map(card => <Card card={card} key={card.id} />)
       ) : (
-          <div className="text-center">No cards found</div>
-        )}
+        <div className="text-center">No cards found</div>
+      )}
     </div>
   );
 };
