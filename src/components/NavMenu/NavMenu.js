cd60f5ab97815c52c1aa4fd91ab8f67fe7d71161
Alejandro <alesan.ca3@gmail.com>
2020-06-08 12:50:49 +0100

    Add highligth navmenu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './navMenu.scss';
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const NavMenu = ({isOpen, toggleNavMenu}) => {
+const NavMenu = ({ isOpen, toggleNavMenu }) => {
   const pages = [
-    {name: 'Home', url: '/'},
-    {name: 'Deck list', url: '/decklist'},
-    {name: 'Wishlist', url: '/wishlist'}
+    { name: 'Home', url: '/' },
+    { name: 'Deck list', url: '/decklist' },
+    { name: 'Wishlist', url: '/wishlist' }
   ];
 
   return (
     <div className={isOpen ? 'nav-menu open' : 'nav-menu'}>
       {pages.map((page, i) => (
-        <Link
-          to={page.url}
+        <NavLink
+          exact to={page.url}
           onClick={toggleNavMenu}
           key={i}
           className="nav-menu__item"
+          activeClassName="selected"
         >
           {page.name}
-        </Link>
+        </NavLink>
       ))}
     </div>
   );
