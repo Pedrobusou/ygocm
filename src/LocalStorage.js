298af7036706ab7e1693504a618ea384c44c15bc
Pedrobusou <pedrobusou@gmail.com>
2020-06-09 23:34:23 +0100

    create first version of the localstorage manager

--- src/LocalStorage.js
+++ src/LocalStorage.js
@@ -1,15 +1,26 @@
-class LocalStorage {
+const PREFIX = 'ygocm_';
 
-    static addCard(id) {
-        var array = [];
-        array.push(id);
-    }
+const handler = (target, action) => {
+  const key = PREFIX + target;
+  let cards = LocalStorage.getCards(key);
 
-    static removeCard(id) {
+  cards = action(cards);
 
-    }
+  localStorage.setItem(key, JSON.stringify(cards));
+};
 
-    static getCard() {
+export class LocalStorage {
+  static add(id) {
+    const addCard = cards => [...cards, id];
+    return {to: target => handler(target, addCard)};
+  }
 
-    }
+  static delete(id) {
+    const deleteCard = cards => cards.filter(c => c !== id);
+    return {from: target => handler(target, deleteCard)};
+  }
+
+  static getCards(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+  }
 }
